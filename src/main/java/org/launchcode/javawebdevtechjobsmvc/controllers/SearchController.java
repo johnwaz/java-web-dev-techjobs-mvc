f181c775fa1b3dfad64f9a2a65e743913028388d
John Wasielewicz <wazjohn@yahoo.com>
2020-10-09 12:27:37 -0500

    Completed the search functionality and displayed search results.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,19 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
+        model.addAttribute("columns", columnChoices);
+        ArrayList<Job> jobs;
+        if (searchType.toLowerCase().equals("all") && searchTerm.equals("")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "Jobs With All: ");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
